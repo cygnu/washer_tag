c55a90682f0391b20f93043e102815588305ffd6
cygnu <crucia8@icloud.com>
2022-04-01 13:07:27 +0900

    feat: Display a sample image

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,40 @@ class WasherQuiz extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade400,
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatelessWidget {
+  const QuizPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Center(
+              child: Image.network(
+                  "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg"),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
