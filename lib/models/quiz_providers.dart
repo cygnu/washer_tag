a0c98d7b610f0ca9ba427ec7db517878abad6b40
cygnu <crucia8@icloud.com>
2022-04-04 02:07:17 +0900

    feat: Add models/quiz_providers.dart

---
+++ lib/models/quiz_providers.dart
@@ -0,0 +1,51 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:state_notifier/state_notifier.dart';
+import 'package:washer_quiz/quiz.dart';
+import 'package:washer_quiz/views/quiz_state.dart';
+
+final quizProvider =
+    StateNotifierProvider.autoDispose<QuizController, QuizState>((ref) {
+  ref.maintainState = true;
+  return QuizController(ref.read);
+});
+
+class QuizController extends StateNotifier<QuizState> with LocatorMixin {
+  QuizController(this._reader)
+      : super(const QuizState(
+          quizNumber: 1,
+          quizOrder: [],
+          correctCount: 0,
+          currentQuizResult: 0,
+          currentQuiz: null,
+        )) {
+    prepare();
+  }
+
+  final Reader _reader;
+
+  void prepare() {
+    List<int> quizOrder = [0, 1];
+    quizOrder.toList().shuffle();
+    state = state.copyWith(
+      quizNumber: 1,
+      quizOrder: quizOrder,
+      correctCount: 0,
+      currentQuiz: quizList[quizOrder[0]],
+      currentQuizResult: 0,
+    );
+  }
+
+  Future<void> answer(int userAnswers) async {
+    if (state.quizNumber! < 5) {
+      state = state.copyWith(
+        quizNumber: state.quizNumber! + 1,
+        currentQuiz: quizList[state.quizOrder![state.quizNumber!]],
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
